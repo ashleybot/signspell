78bb97577c9ecb193f572d51a801ac1bb236c97e
ashleygreer <e.a.greer@gmail.com>
2013-11-08 16:43:19 +0000

    little chat server works

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.listen(port);
--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-var http = require("http");
-var url = require("url");
+var express = require('express');
+var jade = require('jade');
 
-function start(route) {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
-    
-    route(pathname);
-    
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }
-  var port = process.env.PORT || 3000;
-  http.createServer(onRequest).listen(port);
-  console.log("Server has started.");
-}
+var app = express()
+  , http = require('http')
+  , server = http.createServer(app);
+  
+var port = process.env.PORT || 3000;
 
-exports.start = start;
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set("view options", { layout: false });
+app.configure(function() {
+   app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res){
+  res.render('home.jade');
+});
+
+
+
+var io = require('socket.io').listen(server);
+
+
+io.sockets.on('connection', function (socket) {
+   //our other events...
+   socket.on('setPseudo', function (data) {
+    socket.set('pseudo', data);
+   });
+  socket.on('message', function (message) {
+   socket.get('pseudo', function (error, name) {
+        var data = { 'message' : message, pseudo : name };
+        socket.broadcast.emit('message', data);
+        console.log("user " + name + " send this : " + message);
+     })
+   });
+});
+
+
+server.listen(port);
